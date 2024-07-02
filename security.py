ab053c7b4726eb473d0d0c8d43c0ca33e8122488
mianger22 <test666777888999@yandex.ru>
2024-07-02 12:23:49 +0300

    I downloaded the first working version

---
+++ security.py
@@ -0,0 +1,37 @@
+import keyboard
+import ctypes
+import time
+
+# Параметры Message box
+MB_OK = 0
+MB_ICONWARNING = 0x30
+
+def show_alert_message(message):
+    ctypes.windll.user32.MessageBoxW(0, message, "Alert", MB_OK | MB_ICONWARNING)
+
+forbidden_word = "порно"
+typed_buffer = ""
+
+def is_forbidden_word_typed(event):
+    global typed_buffer
+    if event.event_type == keyboard.KEY_DOWN:
+        typed_buffer += event.name
+
+        if "space" in typed_buffer or "\n" in typed_buffer:
+            typed_buffer = ""
+
+        if forbidden_word in typed_buffer:
+            for ch in forbidden_word:
+                keyboard.block_key(ch)
+                keyboard.block_key('space')
+                keyboard.block_key('\n')
+                typed_buffer = ""
+
+                # # Обнуляем буфер при пробеле или переносе строки
+                # if ' ' in typed_buffer or '\n' in typed_buffer:
+                #     typed_buffer = ""
+
+keyboard.hook(is_forbidden_word_typed)
+
+show_alert_message("При вводе слов 18+ клавиатура будет заблокирована. Чтобы возобновить работу, необходимо стереть запрещённое слово и нажать ctrl+c")
+keyboard.wait('ctrl+c')
