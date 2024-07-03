17732fc5389acb074778a15f5cf9a5fa034aaf34
mianger22 <test666777888999@yandex.ru>
2024-07-03 08:01:07 +0300

    I made it so that he erased not one word, but any of the prohibited list

--- delete.py
+++ delete.py
@@ -2,23 +2,24 @@ import keyboard
 import time
 import pyautogui
 
-banned_word = "порн"
+banned_words = ["порн", "эроти", "секс", "мастурб", "дроч", "милф", "инцест", "мачеха", "зрел"]
 typed_chars = []
 
 def on_press(event):
     global typed_chars
-    
+
     # Добавить символ в список
     typed_chars.append(event.name)
-    
-    # Проверить, завершено ли слово
-    if ''.join(typed_chars[-len(banned_word):]) == banned_word:
-        for _ in range(len(banned_word)):
-            print("Слово введено")
-            pyautogui.press('backspace')  # Удалить символы
-        del typed_chars[:]  # Очистить список символов
-    
-    if len(typed_chars) > len(banned_word):
+
+    # Проверить, завершено ли запрещенное слово
+    for banned_word in banned_words:
+        if ''.join(typed_chars[-len(banned_word):]) == banned_word:
+            for _ in range(len(banned_word)):
+                pyautogui.press('backspace')  # Удалить символы
+            del typed_chars[:]  # Очистить список символов
+            break
+
+    if len(typed_chars) > max(map(len, banned_words)):
         typed_chars.pop(0)  # Удаляем самый старый символ, чтобы ограничить длину списка
 
 keyboard.on_press(on_press)
